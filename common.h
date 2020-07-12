3db15aac290aebc4ad96f054b57ac2f6fcd637f9
Alexander Shishkin <alex@ash.works>
2020-07-12 19:06:37 +0300

    Add bug_on() that triggers debugger
    Signed-off-by: Alexander Shishkin <alex@ash.works>

--- common.h
+++ common.h
@@ -9,8 +9,18 @@
 #define EMSCRIPTEN_KEEPALIVE
 #define EM_ASM(x)
 #endif
+
+#ifdef CONFIG_BROWSER
+#define enter_debugger() emscripten_debugger()
+#else
+#include <stdlib.h>
+#define enter_debugger() abort()
+#endif /* CONFIG_BROWSER */
+
 #include "util.h"
 #include "logger.h"
 #include "clap.h"
+
+#define bug_on(_c, args...) do { if ((_c)) { err("condition '" # _c "': " args); enter_debugger();} } while (0)
 
 #endif /* __CLAP_COMMON_H__*/
