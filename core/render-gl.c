69fa1839f9cfbc84edae85eed41396d88b959651
Alexander Shishkin <alex@ash.works>
2022-03-12 18:47:19 +0200

    render/gl: Begin the solemn work of abstracting away OpenGL
    The first order of business is to wrap all GL calls in something that
    checks for errors and lets us know where problems are. While at it,
    we might as well factor out common code sequences and group all relevant
    bits in our own data structures.
    
    We start with struct texture / texture_t, which replaces GUints in the
    rest of the code; texture_t becomes the basic texture unit instead.
    
    When a different renderer comes along, it will only have to deal with
    replacing GL bits inside texture_t with its own, instead of trawling
    through the entire code base.
    
    Signed-off-by: Alexander Shishkin <alex@ash.works>

---
+++ core/render-gl.c
@@ -0,0 +1,138 @@
+#include "display.h"
+#include "logger.h"
+#include "object.h"
+
+#define IMPLEMENTOR
+#include "render.h"
+#undef IMPLEMENTOR
+
+static void texture_drop(struct ref *ref)
+{
+    struct texture *tex = container_of(ref, struct texture, ref);
+    texture_deinit(tex);
+}
+DECLARE_REFCLASS(texture);
+
+int texture_init_target(texture_t *tex, GLuint target)
+{
+    ref_embed(texture, tex);
+    tex->type = GL_UNSIGNED_BYTE;
+    tex->wrap = GL_CLAMP_TO_EDGE;
+    tex->filter = GL_LINEAR;
+    tex->target = target;
+    GL(glPixelStorei(GL_UNPACK_ALIGNMENT, 1));
+    GL(glActiveTexture(tex->target));
+    GL(glGenTextures(1, &tex->id));
+
+    return 0;
+}
+
+int texture_init(texture_t *tex)
+{
+    return texture_init_target(tex, GL_TEXTURE0);
+}
+
+texture_t *texture_clone(texture_t *tex)
+{
+    texture_t *ret = ref_new(texture);
+
+    if (ret) {
+        ret->id     = tex->id;
+        ret->wrap   = tex->wrap; 
+        ret->type   = tex->type;
+        ret->target = tex->target; 
+        ret->filter = tex->filter;
+        ret->width  = tex->width;
+        ret->height = tex->height;
+        ret->format = tex->format;
+        ret->loaded = tex->loaded;
+        tex->loaded = false;
+    }
+
+    return ret;
+}
+
+void texture_deinit(texture_t *tex)
+{
+    if (!tex->loaded)
+        return;
+    GL(glDeleteTextures(1, &tex->id));
+    tex->loaded = false;
+}
+
+void texture_resize(texture_t *tex, unsigned int width, unsigned int height)
+{
+    if (!tex->loaded || (tex->width == width && tex->height == height))
+        return;
+
+    GL(glBindTexture(GL_TEXTURE_2D, tex->id));
+    GL(glTexImage2D(GL_TEXTURE_2D, 0, tex->format, width, height,
+                 0, tex->format, tex->type, NULL));
+    GL(glBindTexture(GL_TEXTURE_2D, 0));
+}
+
+void texture_filters(texture_t *tex, GLint wrap, GLint filter)
+{
+    tex->wrap = wrap;
+    tex->filter = filter;
+}
+
+static void texture_setup_begin(texture_t *tex, void *buf)
+{
+    if (tex->format == GL_DEPTH_COMPONENT)
+        tex->type = GL_FLOAT;
+    GL(glActiveTexture(tex->target));
+    GL(glBindTexture(GL_TEXTURE_2D, tex->id));
+    GL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, tex->wrap));
+    GL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, tex->wrap));
+    GL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, tex->filter));
+    GL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, tex->filter));
+    GL(glTexImage2D(GL_TEXTURE_2D, 0, tex->format, tex->width, tex->height,
+                 0, tex->format, tex->type, buf));
+}
+
+static void texture_setup_end(texture_t *tex)
+{
+    GL(glBindTexture(GL_TEXTURE_2D, 0));
+}
+
+void texture_load(texture_t *tex, GLenum format, unsigned int width, unsigned int height,
+                  void *buf)
+{
+    tex->format = format;
+    tex->width  = width;
+    tex->height = height;
+    texture_setup_begin(tex, buf);
+    texture_setup_end(tex);
+    tex->loaded = true;
+}
+
+void texture_fbo(texture_t *tex, GLuint attachment, GLenum format, unsigned int width,
+                 unsigned int height)
+{
+    tex->format = format;
+    tex->width  = width;
+    tex->height = height;
+    texture_setup_begin(tex, NULL);
+    GL(glFramebufferTexture2D(GL_FRAMEBUFFER, attachment, GL_TEXTURE_2D, tex->id, 0));
+    texture_setup_end(tex);
+    tex->loaded = true;
+}
+
+void texture_done(struct texture *tex)
+{
+    if (!ref_is_static(&tex->ref))
+        ref_put(tex);
+}
+
+GLuint texture_id(struct texture *tex)
+{
+    if (!tex)
+        return -1;
+    return tex->id;
+}
+
+bool texture_loaded(struct texture *tex)
+{
+    return tex->loaded;
+}
--- core/render.h
+++ core/render.h
@@ -1,36 +1,56 @@
 #ifndef __CLAP_RENDER_H__
 #define __CLAP_RENDER_H__
 
+#include <stdlib.h>
 #include "display.h"
 #include "logger.h"
+#include "object.h"
+#include "typedef.h"
 
-static inline void __gl_check_error(void)
+TYPE(texture,
+    struct ref      ref;
+    GLuint          id;
+    GLenum          format;
+    GLenum          type;
+    GLint           wrap;
+    GLint           filter;
+    GLint           target;
+    bool            loaded;
+    unsigned int    width;
+    unsigned int    height;
+);
+
+static inline bool __gl_check_error(const char *str)
 {
     GLenum err;
 
     err = glGetError();
-    if (err != GL_NO_ERROR)
-        err("GL error: %d\n", err);
+    if (err != GL_NO_ERROR) {
+        err("%s: GL error: %d\n", str ? str : "<>", err);
+        abort();
+        return true;
+    }
+
+    return false;
 }
 
-static inline GLuint texture_gen(void)
-{
-    GLuint tex;
+#define GL(__x) do {                    \
+    __x;                                \
+    __gl_check_error(__stringify(__x)); \
+} while (0)
 
-    glGenTextures(1, &tex);
-    // dbg("tex_id >>> %u\n", tex);
-    __gl_check_error();
-
-    return tex;
-}
-
-static inline void texture_del(GLuint tex)
-{
-    if (!tex)
-        return;
-    // dbg("tex_id <<< %u\n", tex);
-    glDeleteTextures(1, &tex);
-    __gl_check_error();
-}
+int texture_init(texture_t *tex);
+int texture_init_target(texture_t *tex, GLuint target);
+void texture_deinit(texture_t *tex);
+void texture_filters(texture_t *tex, GLint wrap, GLint filter);
+void texture_done(texture_t *tex);
+void texture_load(texture_t *tex, GLenum format, unsigned int width, unsigned int height,
+                  void *buf);
+void texture_fbo(texture_t *tex, GLuint attachment, GLenum format, unsigned int width,
+                 unsigned int height);
+void texture_resize(texture_t *tex, unsigned int width, unsigned int height);
+GLuint texture_id(texture_t *tex);
+bool texture_loaded(texture_t *tex);
+texture_t *texture_clone(texture_t *tex);
 
 #endif /* __CLAP_RENDER_H__ */
