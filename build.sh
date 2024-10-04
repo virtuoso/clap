dd026fe774f4473cb3e4a052dc1c215223eaf5d8
Alexander Shishkin <alex@ash.works>
2024-10-04 20:29:37 +0300

    demo: Add ldjam56
    In preparation for ldjam56, allocate a dedicated space for it. While at
    it, get rid of the global and private asset repository and replace it
    with ldjam56-local asset repository that's unburdened of legacy assets
    of questionable licensing.
    
    Signed-off-by: Alexander Shishkin <alex@ash.works>

--- build.sh
+++ build.sh
@@ -12,24 +12,6 @@ cd compile-time
 cmake -B build/rel -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build build/rel
 cd ..
-mkdir -p asset/glsl
-compile-time/build/rel/preprocess_shaders -t glsl -o asset/glsl/ shaders/model
-compile-time/build/rel/preprocess_shaders -t glsl -o asset/glsl/ shaders/ui
-compile-time/build/rel/preprocess_shaders -t glsl -o asset/glsl/ shaders/glyph
-compile-time/build/rel/preprocess_shaders -t glsl -o asset/glsl/ shaders/contrast
-compile-time/build/rel/preprocess_shaders -t glsl -o asset/glsl/ shaders/hblur
-compile-time/build/rel/preprocess_shaders -t glsl -o asset/glsl/ shaders/vblur
-compile-time/build/rel/preprocess_shaders -t glsl -o asset/glsl/ shaders/debug
-compile-time/build/rel/preprocess_shaders -t glsl -o asset/glsl/ shaders/terrain
-mkdir -p asset/glsl-es
-compile-time/build/rel/preprocess_shaders -t glsl-es -o asset/glsl-es/ shaders/model
-compile-time/build/rel/preprocess_shaders -t glsl-es -o asset/glsl-es/ shaders/ui
-compile-time/build/rel/preprocess_shaders -t glsl-es -o asset/glsl-es/ shaders/glyph
-compile-time/build/rel/preprocess_shaders -t glsl-es -o asset/glsl-es/ shaders/contrast
-compile-time/build/rel/preprocess_shaders -t glsl-es -o asset/glsl-es/ shaders/hblur
-compile-time/build/rel/preprocess_shaders -t glsl-es -o asset/glsl-es/ shaders/vblur
-compile-time/build/rel/preprocess_shaders -t glsl-es -o asset/glsl-es/ shaders/debug
-compile-time/build/rel/preprocess_shaders -t glsl-es -o asset/glsl-es/ shaders/terrain
 
 cmake --build build/rel $VERBOSE $OPTS
 cmake --build build/test $VERBOSE $OPTS
@@ -45,5 +27,5 @@ if [ -n "$www_dir" ]; then
 fi
 
 #cp build/emrel/core/onehandclap.{data,html,js,wasm} /var/www/html/cclap
-build/debug/core/server --restart
+build/debug/tools/server/server --restart
 
