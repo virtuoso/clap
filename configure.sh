af7d5e26ab5f1d038dd3b0050bb2a71800a8a36f
Alexander Shishkin <alex@ash.works>
2024-11-29 23:36:55 +0200

    configure.sh: Use cmake presets
    Now that a lot of the boilerplate configuration has been moved to the
    presets, make configure.sh use that.
    
    Signed-off-by: Alexander Shishkin <alex@ash.works>

--- configure.sh
+++ configure.sh
@@ -5,6 +5,7 @@ CLAP_OPTS=""
 [ -n "$emsdk_env_path" ] &&	. $emsdk_env_path
 
 if [ -n "$www_dir" ]; then
+	export WWW_INSTALL_DIR="$www_dir"
 	www_dir="-DCMAKE_INSTALL_PREFIX=$www_dir"
 fi
 
@@ -14,13 +15,14 @@ fi
 if [ -n "$server_ip" ]; then
 	CLAP_OPTS="$CLAP_OPTS -DCLAP_SERVER_IP=$server_ip -DCLAP_BUILD_NETWORKING=ON"
 fi
-cmake -B build/rel -DCMAKE_BUILD_TYPE=Release -DCLAP_BUILD_FINAL=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON $CLAP_OPTS
-cmake -B build/test -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON $CLAP_OPTS
-cmake -B build/debug -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON $CLAP_OPTS
+
+cmake --preset=rel $CLAP_OPTS
+cmake --preset=test $CLAP_OPTS
+cmake --preset=debug $CLAP_OPTS
 
 if which emcmake 1>/dev/null 2>&1; then
-	emcmake cmake -B build/emrel -DCMAKE_BUILD_TYPE=Release -DCLAP_BUILD_FINAL=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON $CLAP_OPTS "$www_dir"
-	emcmake cmake -B build/emtest -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON $CLAP_OPTS "$www_dir"
-	emcmake cmake -B build/emdebug -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON $CLAP_OPTS "$www_dir"
+	cmake --preset=emrel $CLAP_OPTS "$www_dir"
+	cmake --preset=emtest $CLAP_OPTS "$www_dir"
+	cmake --preset=emdebug $CLAP_OPTS "$www_dir"
 fi
 
