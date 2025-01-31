1bf6ecfb27a538c339c18e439e8f9299681ca3b0
Alexander Shishkin <alex@ash.works>
2025-01-31 22:43:49 +0200

    librarian, cmake: Add size field to struct builtin_file
    While all builtin files are strings, strlen() works, but once other
    types of files become cached in builtin_file arrays, their size needs to
    be known. Add it.
    
    Signed-off-by: Alexander Shishkin <alex@ash.works>

--- core/librarian-file.h
+++ core/librarian-file.h
@@ -2,11 +2,13 @@
 #ifndef __CLAP_LIBRARIAN_FILE_H__
 #define __CLAP_LIBRARIAN_FILE_H__
 
+#include <sys/types.h>
 #include <limits.h>
 
 struct builtin_file {
     const char  *name;
     const char  *contents;
+    size_t      size;
 };
 
 #endif /* __CLAP_LIBRARIAN_FILE_H__ */
