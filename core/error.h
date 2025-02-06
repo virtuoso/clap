e14cb41a5cfaad98875e35f7bd863d50b8451845
Alexander Shishkin <alex@ash.works>
2025-02-07 00:24:03 +0200

    error.h: Add error handling std::optional<T> style
    Basically, return a struct with an error and an actual value from
    functions. If error is set, the value is undefined. Add a bunch of
    macros for this:
    
     * cres_ret(T): declare a result structure with value type T
     * cres(T): type name of this structure
     * cres_check(T): cres(T) with must_check
     * cres_error(T, error): define a result structure returning error
     * cres_val(T, value): define a result structure returning value
     * IS_CERR(res): check if passed res is an error
    
    Signed-off-by: Alexander Shishkin <alex@ash.works>

--- core/error.h
+++ core/error.h
@@ -22,4 +22,48 @@ typedef enum cerr {
 #define must_check __attribute__((warn_unused_result))
 #define cerr_check cerr must_check
 
+/*
+ * Returning errors std::optional<T> style
+ */
+
+/* Name of a cres type with value type __type */
+#define cres(__type) cres_ ## __type
+
+/* Declare a cres with value type __type */
+#define cres_ret(__type) \
+    typedef struct cres(__type) { \
+        cerr    err; \
+        __type  val; \
+    } cres(__type)
+
+#define cres_check(__type) cres(__type) must_check
+
+/* Declare cres with int value */
+cres_ret(int);
+
+/* Is cerr or cres an error */
+#define IS_CERR(__x) ( \
+    _Generic(__x, \
+        cerr: (__x), \
+        default: (__x).err \
+    ) != CERR_OK \
+)
+
+/* Return a error */
+#define cres_error(__type, __err) ({ \
+    cres(__type) __res = { \
+        .err = (__err), \
+    }; \
+    __res; \
+})
+
+/* Return a value */
+#define cres_val(__type, __val) ({ \
+    cres(__type) __res = { \
+        .err = CERR_OK, \
+        .val = (__val), \
+    }; \
+    __res; \
+})
+
 #endif /* __CLAP_ERROR_H__ */
