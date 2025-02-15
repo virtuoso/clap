904d55842e423ba31c65d091d801dbed1c535b28
Alexander Shishkin <alex@ash.works>
2025-02-15 03:11:29 +0200

    cmake: Run bootstrap.py from the root of the source tree
    Right now, bootstrap.cmake will run bootstrap.py from the current source
    directory, which is wrong. Make it always run from the top of the source
    tree.
    
    Signed-off-by: Alexander Shishkin <alex@ash.works>

--- scripts/bootstrap.cmake
+++ scripts/bootstrap.cmake
@@ -5,10 +5,10 @@ function(bootstrap_deps bootstrap_file)
         set(PYTHON_EXECUTABLE "python3")
     endif()
 
-    if((NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/deps/.${bootstrap_file}") OR ("${CMAKE_CURRENT_SOURCE_DIR}/CMakeLists.txt" IS_NEWER_THAN "${CMAKE_CURRENT_SOURCE_DIR}/deps/.${bootstrap_file}"))
+    if((NOT EXISTS "${CMAKE_SOURCE_DIR}/deps/.${bootstrap_file}") OR ("${CMAKE_SOURCE_DIR}/CMakeLists.txt" IS_NEWER_THAN "${CMAKE_SOURCE_DIR}/deps/.${bootstrap_file}"))
         execute_process(
-            COMMAND ${PYTHON_EXECUTABLE} "bootstrap.py" --bootstrap-file "${CMAKE_CURRENT_SOURCE_DIR}/deps/${bootstrap_file}"
-            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            COMMAND ${PYTHON_EXECUTABLE} "bootstrap.py" --bootstrap-file "${CMAKE_SOURCE_DIR}/deps/${bootstrap_file}"
+            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
             RESULT_VARIABLE bootstrap_result
         )
         if (NOT (${bootstrap_result} EQUAL 0))
