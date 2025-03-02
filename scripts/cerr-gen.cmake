e1715218e6c9cd273c201e19aafa84d5c29a1db8
Alexander Shishkin <alex@ash.works>
2025-03-02 21:44:24 +0200

    cmake: Generate cerrs.h during build instead of configuration
    Currently, cerrs.h is generated at configuration stage, so you'd need to
    touch CMakeLists.txt or something like that to re-generate it. Instead,
    add a proper custom target with a dependency on error.h, from which it
    is generated.
    
    Signed-off-by: Alexander Shishkin <alex@ash.works>

--- scripts/cerr-gen.cmake
+++ scripts/cerr-gen.cmake
@@ -4,11 +4,11 @@ if (NOT CLAP_BUILD_FINAL)
     set(debug_info " .line = __LINE__, .mod = MODNAME,")
 endif ()
 
-file(READ "${CMAKE_CURRENT_SOURCE_DIR}/error.h" error_src)
+file(READ "${dir}/error.h" error_src)
 string(REGEX REPLACE "^.*typedef enum cerr_enum {(.*)} cerr_enum.*$" "\\1" error_out ${error_src})
-string(REGEX REPLACE " *_([A-Z_]*)[\- =0-9]+,\n"
+string(REGEX REPLACE " *_([A-Z_]*)[- =0-9]+,\n"
                      "#define \\1 ((const cerr){ .err = _\\1,${debug_info} })\n" error_out ${error_out})
-file(WRITE  ${CMAKE_CURRENT_BINARY_DIR}/cerrs.h "#ifndef __CLAP_CERRS_H__\n")
-file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/cerrs.h "#define __CLAP_CERRS_H__\n")
-file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/cerrs.h "${error_out}\n")
-file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/cerrs.h "#endif /* __CLAP_CERRS_H__ */\n")
+file(WRITE  "${output}" "#ifndef __CLAP_CERRS_H__\n")
+file(APPEND "${output}" "#define __CLAP_CERRS_H__\n")
+file(APPEND "${output}" "${error_out}\n")
+file(APPEND "${output}" "#endif /* __CLAP_CERRS_H__ */\n")
