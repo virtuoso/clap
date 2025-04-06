8df2af86d5a2b13639907163f616162095f5267e
Alexander Shishkin <alex@ash.works>
2025-04-06 14:26:04 +0300

    ui: Display clap version string
    Signed-off-by: Alexander Shishkin <alex@ash.works>

--- core/ui.c
+++ core/ui.c
@@ -1756,7 +1756,7 @@ cerr ui_init(struct ui *ui, clap_context *clap_ctx, int width, int height)
     uie1->on_click = build_onclick;
 
 #ifndef CONFIG_FINAL
-    build_uit = ui_printf(ui, font, uie1, (vec4){ 0.7, 0.7, 0.7, 1.0 }, 0, "%s", build_date);
+    build_uit = ui_printf(ui, font, uie1, (vec4){ 0.7, 0.7, 0.7, 1.0 }, 0, "%s @%s", clap_version, build_date);
 #endif
 
     const char *pocket_textures[] = { "apple.png", "mushroom thumb.png" };
