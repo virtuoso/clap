8a7bc3a1826e8bf27a943a6a110d77a2cd1c8b4f
Alexander Shishkin <alex@ash.works>
2025-06-21 10:46:39 +0300

    transform: Add rotation and corresponding APIs
    Add rotation component to transform_t, a getter, a setter and a matrix
    rotation function.
    
    Signed-off-by: Alexander Shishkin <alex@ash.works>

--- core/transform.c
+++ core/transform.c
@@ -3,6 +3,8 @@
 #include "transform.h"
 #undef IMPLEMENTOR
 
+#include "util.h"
+
 void transform_set_pos(transform_t *xform, const vec3 pos)
 {
     vec3_dup(xform->pos, pos);
@@ -26,4 +28,32 @@ const float *transform_pos(transform_t *xform, float *pos)
 void transform_translate_mat4x4(transform_t *xform, mat4x4 m)
 {
     mat4x4_translate_in_place(m, xform->pos[0], xform->pos[1], xform->pos[2]);
+}
+
+void transform_set_angles(transform_t *xform, const float angles[3], bool degrees)
+{
+    vec3_dup(
+        xform->angles,
+        degrees ? (vec3) {
+            to_radians(angles[0]),
+            to_radians(angles[1]),
+            to_radians(angles[2])
+        } : angles
+    );
+}
+
+void transform_rotation(transform_t *xform, float angles[3], bool degrees)
+{
+    if (degrees)
+        for (int i = 0; i < 3; i++)
+            angles[i] = to_degrees(xform->angles[i]);
+    else
+        vec3_dup(angles, xform->angles);
+}
+
+void transform_rotate_mat4x4(transform_t *xform, mat4x4 m)
+{
+    mat4x4_rotate_X(m, m, xform->angles[0]);
+    mat4x4_rotate_Y(m, m, xform->angles[1]);
+    mat4x4_rotate_Z(m, m, xform->angles[2]);
 }
--- core/transform.h
+++ core/transform.h
@@ -7,11 +7,15 @@
 
 TYPE(transform,
     vec3    pos;
+    float   angles[3];
 );
 
 void transform_set_pos(transform_t *xform, const vec3 pos);
 const float *transform_move(transform_t *xform, const vec3 off);
 const float *transform_pos(transform_t *xform, float *pos);
 void transform_translate_mat4x4(transform_t *xform, mat4x4 m);
+void transform_set_angles(transform_t *xform, const float angles[3], bool degrees);
+void transform_rotation(transform_t *xform, float angles[3], bool degrees);
+void transform_rotate_mat4x4(transform_t *xform, mat4x4 m);
 
 #endif /* __CLAP_TRANSFORM_H__ */
