a4adf44b13ebc8dd97d4824d266d8813310c678a
Alexander Shishkin <alex@ash.works>
2025-07-01 20:56:37 +0300

    shader(s): Add uniforms for procedural material generation
    Add uniforms necessary for FBM-based generation of per-fragment metallic
    and roughness values: ceiling, number of octaves, amplitude scale per
    octave and vertex coordinates scale.
    
    Signed-off-by: Alexander Shishkin <alex@ash.works>

--- shaders/ubo_material.glsl
+++ shaders/ubo_material.glsl
@@ -6,6 +6,15 @@ layout (std140, binding = UBO_BINDING_material) uniform material {
     float shine_damper;
     float roughness;
     float metallic;
+    /* procedural roughness/metallic parameters, see material type */
+    float roughness_ceil;
+    float roughness_amp;
+    int   roughness_oct;
+    float roughness_scale;
+    float metallic_ceil;
+    float metallic_amp;
+    int   metallic_oct;
+    float metallic_scale;
 };
 
 #endif /* SHADERS_UBO_MATERIAL_GLSL */
