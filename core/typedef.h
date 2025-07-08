1ebfe8f0c5fc0aa3df22f445460eb0bcc1bbda09
Alexander Shishkin <alex@ash.works>
2025-07-08 20:41:28 +0300

    typedef: Add TYPE_FORWARD() for forward type declarations
    In a non-trivial header, data types refer to one another, and a forward
    type declaration is necessary. Add TYPE_FORWARD() for this.
    
    Signed-off-by: Alexander Shishkin <alex@ash.works>

--- core/typedef.h
+++ core/typedef.h
@@ -6,15 +6,21 @@
 #ifdef IMPLEMENTOR
 #define TYPE(__type, __def) \
     typedef struct __type { __def } __type ## _t;
+#define TYPE_FORWARD(__type) \
+    typedef struct __type __type ## _t;
 #else
 # ifdef CLAP_DEBUG
 #define TYPE(__type, __def) \
     typedef struct __type ## _impl { __def } __type ## _t;
+#define TYPE_FORWARD(__type) \
+    typedef struct __type ## _impl __type ## _t;
 # else
 #define TYPE(__type, __def) \
     typedef struct __type { char __res[ sizeof(struct { __def }) ]; } __type ## _t; \
     struct __type ## _impl { __def }; \
     static_assert(sizeof(struct __type ## _impl) == sizeof(__type ## _t), \
                   # __type "_t doesn't match implementation");
+#define TYPE_FORWARD(__type) \
+    typedef struct __type __type ## _t;
 # endif
 #endif
