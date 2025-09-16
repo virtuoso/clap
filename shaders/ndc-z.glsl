b9fb286554bddc1eba05604323b0974f3ee79db4
Alexander Shishkin <alex@ash.works>
2025-09-16 20:21:01 +0300

    core, shaders: Add a macro for [0.0, 1.0] Z range of NDCs
    Add CONFIG_NDC_ZERO_ONE for render backends that have Z in [0.0, 1.0]
    range in NDC, as opposed to [-1.0, 1.0]; basically everything that's not
    OpenGL. Add a GLSL helper to return a Z values appropriate for the
    selected backend.
    
    Signed-off-by: Alexander Shishkin <alex@ash.works>

---
+++ shaders/ndc-z.glsl
@@ -0,0 +1,12 @@
+#ifndef SHADERS_NDC_Z_GLSL
+#define SHADERS_NDC_Z_GLSL
+
+#include "config.h"
+
+#ifdef CONFIG_NDC_ZERO_ONE
+float convert_ndc_z(float z)    { return clamp(z, 0.0, 1.0); }
+#else
+float convert_ndc_z(float z)    { return clamp(z * 2.0 - 1.0, -1.0, 1.0); }
+#endif /* !CONFIG_NDC_ZERO_ONE */
+
+#endif /* SHADERS_NDC_Z_GLSL */
