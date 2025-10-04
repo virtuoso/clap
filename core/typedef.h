c31133333250beae01c4d42dfeb670a60c28caa7
Alexander Shishkin <alex@ash.works>
2025-10-04 08:46:06 +0300

    typedef: Fix alignment
    The opaque type definition hid a monumental heisenbug: their natural
    alignment is 1, whereas real types' alignment is 4/8/16, which
    introduced sneaky unaligned accesses to the code, causing subtle bugs,
    each depending on what is doing the accessing.
    
    Fix this by aligning opaque types the same way as the originals.
    
    Signed-off-by: Alexander Shishkin <alex@ash.works>

--- core/typedef.h
+++ core/typedef.h
@@ -16,8 +16,9 @@
     typedef struct __type ## _impl __type ## _t;
 # else
 #define TYPE(__type, __def) \
-    typedef struct __type { char __res[ sizeof(struct { __def }) ]; } __type ## _t; \
     struct __type ## _impl { __def }; \
+    typedef struct __type { char __res[ sizeof(struct { __def }) ]; } __type ## _t \
+        __attribute__((aligned(__alignof__(struct __type ## _impl)))); \
     static_assert(sizeof(struct __type ## _impl) == sizeof(__type ## _t), \
                   # __type "_t doesn't match implementation");
 #define TYPE_FORWARD(__type) \
