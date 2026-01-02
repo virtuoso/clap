0b6406aa35c4bf34361a5bb48f925fea6979b5be
Alexander Shishkin <alex@ash.works>
2026-01-02 22:10:21 +0200

    render-gl: Move std140 stuff to render-common.c
    The UBO packing is mostly similar between GL (std140) and Metal. Move it
    to render-common.c.
    
    Signed-off-by: Alexander Shishkin <alex@ash.works>

--- core/render-common.c
+++ core/render-common.c
@@ -3,6 +3,128 @@
 #include "render.h"
 #undef IMPLEMENTOR
 
+/****************************************************************************
+ * UBO packing: std140 and the like
+ ****************************************************************************/
+
+/* Get std140 storage size for a given type */
+static inline size_t type_storage_size(data_type type)
+{
+    size_t storage_size = data_type_size(type);
+    if (!storage_size) {
+        clap_unreachable();
+        return 0;
+    }
+
+    /* Matrices are basically arrays of vec4 (technically, vecN padded to 16 bytes) */
+    switch (type) {
+        case DT_MAT2:
+            storage_size = 2 * data_type_size(DT_VEC4); /* vec2 padded to vec4 */
+            break;
+        case DT_MAT3:
+            storage_size = 3 * data_type_size(DT_VEC4); /* vec3 padded to vec4 */
+            break;
+        /* And what's left is mat4, which is perfect as it is */
+        default:
+            /*
+             * Scalars are not padded to 16 bytes unless they are followed by
+             * compound types, which we don't know about here; instead,
+             * uniform_buffer_set() handles the std140 compliant offset alignment.
+             */
+            break;
+    }
+
+    return storage_size;
+}
+
+/*
+ * Calculate uniform @offset within a UBO, its total @size and set its @value
+ * if @value is not NULL
+ */
+cerr uniform_buffer_set(uniform_buffer_t *ubo, data_type type, size_t *offset, size_t *size,
+                        unsigned int count, const void *value)
+{
+    size_t elem_size = data_type_size(type);       /* C ABI element size */
+    size_t storage_size = type_storage_size(type); /* Metal-aligned size */
+    bool dirty = ubo->dirty;
+    cerr err = CERR_OK;
+
+    if (!elem_size || !storage_size)
+        return CERR_INVALID_ARGUMENTS;
+
+    /* These are only used if @value is set */
+    const char *src = (const char *)value;
+    char *dst = src ? (char *)ubo->data + *offset : NULL;
+
+    *offset = *size;
+
+    /*
+     * Individual scalars are *not* padded to 16 bytes, unless they're in
+     * an array; compound types are aligned to a 16 bype boundary even if
+     * they follow non-padded scalars
+     */
+    if (storage_size < 16 && count > 1)
+        storage_size = 16;
+
+    /*
+     * vec2 is aligned on 8-bype boundary;
+     * non-arrayed scalars have maximum storage size of 4, if something
+     * larger is following a non-padded offset, it needs to be padded first
+     */
+    if (storage_size > 4 && *offset % 16)
+        *offset = round_up(*offset, 16);
+
+    *size = *offset;
+
+    /*
+     * Copy elements from a C array to a UBO array. Metal UBO layout, like
+     * std140, uses fun alignments for various types that don't match C at
+     * all, they need to be copied one at a time.
+     */
+    for (unsigned int i = 0; i < count; i++) {
+        if (value) {
+            /* If we overshoot, the buffer is still dirty, return straight away */
+            if (*size + storage_size > ubo->size) {
+                err = CERR_BUFFER_OVERRUN;
+                goto out;
+            }
+
+            if (type == DT_MAT2 || type == DT_MAT3) {
+                /* Manually copy row-by-row with padding */
+                bool is_mat3 = type == DT_MAT3;
+                int rows = is_mat3 ? 3 : 2;
+                /* Source row: DT_VEC2 or DT_VEC3 */
+                size_t row_size = data_type_size(is_mat3 ? DT_VEC3 : DT_VEC2);
+                /* Destination row: DT_VEC4 */
+                size_t row_stride = type_storage_size(DT_VEC4);
+                for (int r = 0; r < rows; r++) {
+                    if (memcmp(dst, src, row_size)) {
+                        memcpy(dst, src, row_size);
+                        dirty = true;
+                    }
+                    src += row_size;             /* Move to next row */
+                    dst += row_stride;           /* Move to next aligned row */
+                }
+            } else {
+                /* Only update the target value if it changed */
+                if (memcmp(dst, src, elem_size)) {
+                    /* Copy only valid bytes */
+                    memcpy(dst, src, elem_size);
+                    dirty = true;
+                }
+                src += elem_size;            /* Move to next element (C ABI aligned) */
+                dst += storage_size;         /* Move to next element (std140 aligned) */
+            }
+        }
+        *size += storage_size;
+    }
+
+out:
+    ubo->dirty = dirty;
+
+    return err;
+}
+
 static const char *texture_format_strings[TEX_FMT_MAX] = {
         [TEX_FMT_R8]        = "R8",
         [TEX_FMT_R16F]      = "R16F",
