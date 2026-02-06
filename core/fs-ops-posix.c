717faac2ee7f0df261fb868c256082ab825a7c41
Alexander Shishkin <alex@ash.works>
2026-02-06 02:19:24 +0200

    fs-ops/posix: Add file operations
    Currently struct fs_ops only has directory operations. Add file
    open/close/read/write/seek to struct fs_ops and implement their posix
    versions.
    
    Signed-off-by: Alexander Shishkin <alex@ash.works>

--- core/fs-ops-posix.c
+++ core/fs-ops-posix.c
@@ -95,10 +95,111 @@ static int fs_posix_dirent_cmp(const void *a, const void *b)
     return strcasecmp(ea->name, eb->name);
 }
 
+static cresp(void) fs_posix_open(const char *path, enum fs_mode mode, bool create, bool exclusive, bool binary)
+{
+    FILE *f = NULL;
+
+    if (!create) {
+        f = fopen(path, "r");
+        if (!f)
+            return cresp_error_cerr(void, errno_to_cerr(errno));
+    }
+
+    char mode_buf[8];
+    const char *m;
+
+    if (binary && mode == FS_APPEND)
+        return cresp_error(void, CERR_INVALID_ARGUMENTS);
+
+    switch (mode) {
+    case FS_READ:   m = binary ? FOPEN_RB : "r"; break;
+    case FS_WRITE:  m = binary ? FOPEN_WB : "w"; break;
+    case FS_APPEND: m = "a"; break;
+    case FS_BOTH:   m = "w+"; break;
+    default:        return cresp_error(void, CERR_INVALID_ARGUMENTS);
+    }
+
+    snprintf(mode_buf, sizeof(mode_buf), "%s%s%s", m,
+             (binary && mode == FS_BOTH) ? "b" : "",
+             (mode != FS_READ && exclusive) ? "x" : "");
+
+    if (create) {
+        f = fopen(path, mode_buf);
+    } else {
+        f = freopen(path, mode_buf, f);
+    }
+
+    if (!f)
+        return cresp_error_cerr(void, errno_to_cerr(errno));
+
+    if (binary)
+        compat_set_binary(f);
+
+    return cresp_val(void, f);
+}
+
+static cerr fs_posix_close(void *handle)
+{
+    if (!handle)
+        return CERR_OK;
+
+    if (fclose(handle) != 0)
+        return errno_to_cerr(errno);
+
+    return CERR_OK;
+}
+
+static cres(size_t) fs_posix_read(void *handle, void *buf, size_t size)
+{
+    size_t read = fread(buf, 1, size, handle);
+
+    if (read < size && ferror(handle))
+        return cres_error_cerr(size_t, errno_to_cerr(errno));
+
+    return cres_val(size_t, read);
+}
+
+static cres(size_t) fs_posix_write(void *handle, const void *buf, size_t size)
+{
+    size_t written = fwrite(buf, 1, size, handle);
+
+    if (written < size && ferror(handle))
+        return cres_error_cerr(size_t, errno_to_cerr(errno));
+
+    return cres_val(size_t, written);
+}
+
+static cerr fs_posix_seek(void *handle, long offset, enum fs_seek_origin origin)
+{
+    int whence = SEEK_SET;
+
+    switch (origin) {
+    case FS_SEEK_SET:
+        whence = SEEK_SET;
+        break;
+    case FS_SEEK_CUR:
+        whence = SEEK_CUR;
+        break;
+    case FS_SEEK_END:
+        whence = SEEK_END;
+        break;
+    }
+
+    if (fseek(handle, offset, whence) != 0)
+        return errno_to_cerr(errno);
+
+    return CERR_OK;
+}
+
 const struct fs_ops fs_ops_posix = {
     .get_cwd    = fs_posix_get_cwd,
     .open_dir   = fs_posix_open_dir,
     .read_dir   = fs_posix_read_dir,
     .close_dir  = fs_posix_close_dir,
     .dirent_cmp = fs_posix_dirent_cmp,
+    .open       = fs_posix_open,
+    .close      = fs_posix_close,
+    .read       = fs_posix_read,
+    .write      = fs_posix_write,
+    .seek       = fs_posix_seek,
 };
--- core/fs-ops.h
+++ core/fs-ops.h
@@ -18,12 +18,43 @@ struct fs_dirent {
 };
 
 /**
+ * enum fs_mode - file open mode
+ * @FS_READ:   read only
+ * @FS_WRITE:  write only
+ * @FS_APPEND: append only
+ * @FS_BOTH:   read and write
+ */
+enum fs_mode {
+    FS_READ,
+    FS_WRITE,
+    FS_APPEND,
+    FS_BOTH,
+};
+
+/**
+ * enum fs_seek_origin - file seek origin
+ * @FS_SEEK_SET: beginning of file
+ * @FS_SEEK_CUR: current position
+ * @FS_SEEK_END: end of file
+ */
+enum fs_seek_origin {
+    FS_SEEK_SET,
+    FS_SEEK_CUR,
+    FS_SEEK_END,
+};
+
+/**
  * struct fs_ops - filesystem access vtable
  * @get_cwd:   fill @out_path with the current working directory
  * @open_dir:  open a directory stream for @path
  * @read_dir:  iterate over a directory stream, returns CERR_EOF at end
  * @close_dir: close a directory stream opened by @open_dir
  * @dirent_cmp: qsort-compatible comparison function for fs_dirent entries
+ * @open:      open a file
+ * @close:     close a file
+ * @read:      read from file
+ * @write:     write to file
+ * @seek:      seek in file
  */
 struct fs_ops {
     cerr_check          (*get_cwd)(char out_path[PATH_MAX]);
@@ -31,6 +62,11 @@ struct fs_ops {
     cerr_check          (*read_dir)(void *dir, struct fs_dirent *out);
     void                (*close_dir)(void *dir);
     int                 (*dirent_cmp)(const void *a, const void *b);
+    cresp_check(void)   (*open)(const char *path, enum fs_mode mode, bool create, bool exclusive, bool binary);
+    cerr_check          (*close)(void *handle);
+    cres_check(size_t)  (*read)(void *handle, void *buf, size_t size);
+    cres_check(size_t)  (*write)(void *handle, const void *buf, size_t size);
+    cerr_check          (*seek)(void *handle, long offset, enum fs_seek_origin origin);
 };
 
 extern const struct fs_ops fs_ops_posix;
