45deed2b1f59e301f16690d1c61444178ef32553
Alexander Shishkin <alex@ash.works>
2026-03-21 05:29:21 +0200

    shaders: Add linearize-depth.glsl
    Add linearize_depth_ndc() and linearize_depth() utility functions to be
    used across multiple shaders, some of which already carry private
    versions of these. Unlike the pre-existing ones, these take into account
    the NDC Z range.
    
    Signed-off-by: Alexander Shishkin <alex@ash.works>

---
+++ shaders/linearize-depth.glsl
@@ -0,0 +1,21 @@
+#ifndef SHADERS_LINEARIZE_DEPTH_GLSL
+#define SHADERS_LINEARIZE_DEPTH_GLSL
+
+#include "config.h"
+#include "ndc-z.glsl"
+
+float linearize_depth_ndc(float ndc_z, float near_plane, float far_plane)
+{
+#ifdef CONFIG_NDC_ZERO_ONE
+    return near_plane * far_plane / (far_plane - ndc_z * (far_plane - near_plane));
+#else /* !CONFIG_NDC_ZERO_ONE */
+    return (2.0 * near_plane * far_plane) / (far_plane + near_plane - ndc_z * (far_plane - near_plane));
+#endif /* !CONFIG_NDC_ZERO_ONE */
+}
+
+float linearize_depth(float depth, float near_plane, float far_plane)
+{
+    return linearize_depth_ndc(convert_to_ndc_z(depth), near_plane, far_plane);
+}
+
+#endif /* SHADERS_LINEARIZE_DEPTH_GLSL */
