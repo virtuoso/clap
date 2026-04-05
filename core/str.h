38655851c9a5dffea811c72747fda150fe9811ad
Alexander Shishkin <alex@ash.works>
2026-04-06 01:42:42 +0300

    str: Add support for const strings to string_view
    Currently, string_view can't be used with const strings, because it's
    internal buffer pointer is not const. It is, however, still useful for
    const strings.
    
    To support them, do the following:
     - strip off the const qualifier in declare_sv*()
     - add helper macros to detect const strings
     - for const strings, set string_view::cap to zero to indicate that this
       string_view is not writable
     - explicitly check for zero capacity in sv_append() and exit early with
       an error
     - add a comment explaining this and the intention to make string_view
       an opaque type in the future, so that users can't write to its data
       directly.
    
    Signed-off-by: Alexander Shishkin <alex@ash.works>

--- core/str.h
+++ core/str.h
@@ -12,16 +12,33 @@ typedef struct string_view {
 #define type_as_expr(...) (*(typeof(__VA_ARGS__) *)nullptr)
 
 #define str_is_ptr(x) _Generic(type_as_expr(&(x)), \
-    char **:    true, \
-    default:    false \
+    const char **:  true, \
+    char **:        true, \
+    default:        false \
 )
+
+#define _str_is_const(x) _Generic(&(x), \
+    const char **:      true, \
+    const char (*)[]:   true, \
+    default:            false \
+)
+
+#define str_is_const(x) (__builtin_constant_p((x)) ? true : _str_is_const((x)))
 
 #define sv(_str)    (_str ## _sv)
 
+// Note: in order for this to be usable on constant strings, we need to strip
+// away const qualifier, so that string_view::data can still be set. We
+// compensate for this by setting capacity to 0, so sv_append() won't try to
+// modify the underlying string. However, this is aliasing, and since
+// string_view::data is visible, it can be used to write to constant strings,
+// or at least attempt to.
+// TODO: make string_view opaque, so only dedicated functions can operate on
+// it.
 #define declare_sv_from(_sv, _str) \
     string_view sv(_sv) = { \
-        .data   = (_str), \
-        .cap    = str_is_ptr((_str)) ? 0 : sizeof((_str)), \
+        .data   = (char *)(_str), \
+        .cap    = str_is_const((_str)) ? 0 : (str_is_ptr((_str)) ? strlen((_str)) + 1 : sizeof((_str))), \
         .end    = str_is_ptr((_str)) ? strlen((_str)) : sizeof((_str)) - 1, \
     }
 
@@ -37,6 +54,13 @@ static inline void sv_reset(string_view *sv)    { sv->end = 0; }
 
 static inline cres(size_t) sv_append(string_view *sv, const char *fmt, ...)
 {
+    if (!sv->cap)
+        return cres_error(
+            size_t,
+            CERR_INVALID_OPERATION,
+            .fmt = "writing to a const string"
+        );
+
     if (sv->end >= sv->cap - 1) {
         sv->end = sv->cap - 1;
         return cres_error(size_t, CERR_TOO_LARGE);
