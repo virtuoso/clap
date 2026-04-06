66950868edc67af055a8b741b36f1cddbd7d18ca
Alexander Shishkin <alex@ash.works>
2026-04-06 03:36:54 +0300

    render-common: Factor out texture formats' queries
    Texture formats' names, sizes and number of components easily lift out
    into a dedicated TU that doesn't depend on much else, this is just
    querying static data. Factor them out into render-texture.c, which is
    also a start of texture lookup tables that will replace the switch(fmt)
    based functions in the existing rendering backends, as they are error
    prone, annoying to maintain and in general an antipattern.
    
    Signed-off-by: Alexander Shishkin <alex@ash.works>

--- core/render-common.c
+++ core/render-common.c
@@ -186,50 +186,6 @@ void textures_done(void)
     texture_done(&_transparent_pixel);
 }
 
-static const struct {
-    const char  *name;
-    size_t      comp_sz;
-    size_t      nr_comps;
-} texture_format_desc[TEX_FMT_MAX] = {
-    [TEX_FMT_R8]        = { .name = "R8",           .comp_sz = 1, .nr_comps = 1 },
-    [TEX_FMT_R16F]      = { .name = "R16F",         .comp_sz = 2, .nr_comps = 1 },
-    [TEX_FMT_R32F]      = { .name = "R32F",         .comp_sz = 4, .nr_comps = 1 },
-    [TEX_FMT_RG8]       = { .name = "RG8",          .comp_sz = 1, .nr_comps = 2 },
-    [TEX_FMT_RG16F]     = { .name = "RG16F",        .comp_sz = 2, .nr_comps = 2 },
-    [TEX_FMT_RG32F]     = { .name = "RG32F",        .comp_sz = 4, .nr_comps = 2 },
-    [TEX_FMT_RGBA8]     = { .name = "RGBA8",        .comp_sz = 1, .nr_comps = 4 },
-    [TEX_FMT_RGB8]      = { .name = "RGB8",         .comp_sz = 1, .nr_comps = 3 },
-    [TEX_FMT_RGBA8_SRGB]= { .name = "RGBA8_sRGB",   .comp_sz = 1, .nr_comps = 4 },
-    [TEX_FMT_RGB8_SRGB] = { .name = "RGB8_sRGB",    .comp_sz = 1, .nr_comps = 3 },
-    [TEX_FMT_RGBA16F]   = { .name = "RGBA16F",      .comp_sz = 2, .nr_comps = 4 },
-    [TEX_FMT_RGB16F]    = { .name = "RGB16F",       .comp_sz = 2, .nr_comps = 3 },
-    [TEX_FMT_RGBA32F]   = { .name = "RGBA32F",      .comp_sz = 4, .nr_comps = 4 },
-    [TEX_FMT_RGB32F]    = { .name = "RGB32F",       .comp_sz = 4, .nr_comps = 3 },
-    [TEX_FMT_R32UI]     = { .name = "R32UI",        .comp_sz = 4, .nr_comps = 1 },
-    [TEX_FMT_RG32UI]    = { .name = "RG32UI",       .comp_sz = 4, .nr_comps = 2 },
-    [TEX_FMT_RGBA32UI]  = { .name = "RGBA32UI",     .comp_sz = 4, .nr_comps = 4 },
-    [TEX_FMT_DEPTH32F]  = { .name = "DEPTH32F",     .comp_sz = 4, .nr_comps = 1 },
-    [TEX_FMT_DEPTH24F]  = { .name = "DEPTH24F",     .comp_sz = 3, .nr_comps = 1 },
-    [TEX_FMT_DEPTH16F]  = { .name = "DEPTH16F",     .comp_sz = 2, .nr_comps = 1 },
-};
-
-const char *texture_format_string(texture_format fmt)
-{
-    if (fmt >= TEX_FMT_MAX) return "<invalid>";
-    return texture_format_desc[fmt].name;
-}
-
-size_t texture_format_comp_size(texture_format fmt)
-{
-    if (fmt >= TEX_FMT_MAX) return 0;
-    return texture_format_desc[fmt].comp_sz;
-}
-
-size_t texture_format_nr_comps(texture_format fmt)
-{
-    if (fmt >= TEX_FMT_MAX) return 0;
-    return texture_format_desc[fmt].nr_comps;
-}
 
 #ifndef CONFIG_FINAL
 #include "ui-debug.h"
