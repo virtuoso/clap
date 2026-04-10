50a059e225ce9abb8e9fbd6ffb262b26e31f3ccd
Alexander Shishkin <alex@ash.works>
2026-04-10 23:59:04 +0300

    noise: Add periodic gradient-noise 3D texture baker
    New core/noise.{c,h} with a seamless, tileable gradient-noise baker:
    value-noise fBm sampled with a central-difference gradient, packed to
    RGBA8 and uploaded into a TEX_3D texture. Infrastructure for upcoming
    material noise normals and 3D fog.
    
    hash31() is exposed inline for reuse by other noise helpers.
    
    Signed-off-by: Alexander Shishkin <alex@ash.works>

---
+++ core/noise.h
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+#ifndef __CLAP_NOISE_H__
+#define __CLAP_NOISE_H__
+
+#include <stdint.h>
+#include "error.h"
+#include "render.h"
+
+static inline float hash31(int x, int y, int z, uint32_t seed)
+{
+    uint32_t h = (uint32_t)x * 374761393u
+               + (uint32_t)y * 668265263u
+               + (uint32_t)z * 362437u
+               + seed * 2246822519u;
+    h = (h ^ (h >> 13)) * 1274126177u;
+    return (h ^ (h >> 16)) * (1.0f / 4294967296.0f);
+}
+
+cresp(void) noise_grad3d_bake_rgba8(size_t size, int octaves, float lacunarity, float gain,
+                                    float period_units, uint32_t seed);
+
+cerr noise_grad3d_bake_rgba8_tex(renderer_t *r, texture_t *tex, int size, int octaves, float lacunarity,
+                                 float gain, float period_units, uint32_t seed);
+
+#endif /* __CLAP_NOISE_H__ */
